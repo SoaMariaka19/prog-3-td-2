9f944c33e50bf78ba01ebad555bb9cb63f7930fd
soamariaka <hei.soa.2@gmail.com>
2023-02-09 11:57:15 +0300

    feat: add Match test

---
+++ src/main/java/app/foot/exception/ExceptionHandlerManager.java
@@ -0,0 +1,48 @@
+package app.foot.exception;
+
+import app.foot.model.ErrorDetailsFormat;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestControllerAdvice
+@ControllerAdvice
+@Slf4j
+public class ExceptionHandlerManager extends ResponseEntityExceptionHandler {
+  @ExceptionHandler({ BadRequestException.class })
+  public ResponseEntity<Object> handleMethodArgumentTypeMismatch(
+          BadRequestException ex, WebRequest request) {
+    String error =
+            ex.getMessage() + " should be of type " + ex.getCause();
+
+    ErrorDetailsFormat apiError =
+            new ErrorDetailsFormat(HttpStatus.BAD_REQUEST, ex.getLocalizedMessage(), error);
+    return new ResponseEntity<>(
+            apiError, new HttpHeaders(), apiError.getStatus());
+  }
+  @ExceptionHandler({ InternalServerException.class })
+  public ResponseEntity<Object> handleMethodServerError(
+          InternalServerException ex, WebRequest request) {
+    String error =
+            request.getHeader(String.valueOf(ex.getMessage()));
+
+    ErrorDetailsFormat apiError =
+            new ErrorDetailsFormat(HttpStatus.INTERNAL_SERVER_ERROR , ex.getLocalizedMessage(), error);
+    return new ResponseEntity<>(
+            apiError, new HttpHeaders(), apiError.getStatus());
+  }
+}
